784eb7383f9162daca796da07c7bc3f9c679ee52
sh0e1 <sh0e1@users.noreply.github.com>
2025-01-17 23:53:21 +0900

    add keymap for timer

--- internal/cmd/timer/timer.go
+++ internal/cmd/timer/timer.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/bubbles/timer"
 	tea "github.com/charmbracelet/bubbletea"
@@ -22,18 +24,42 @@ func NewCommand() *cobra.Command {
 	return cmd
 }
 
+const timeout = 25 * time.Minute
+
 type model struct {
-	spinner  spinner.Model
 	timer    timer.Model
+	spinner  spinner.Model
+	keymap   keymap
+	help     help.Model
 	quitting bool
 	err      error
 }
 
+type keymap struct {
+	start key.Binding
+	stop  key.Binding
+	reset key.Binding
+	quit  key.Binding
+}
+
+func (k keymap) bindings() []key.Binding {
+	return []key.Binding{k.start, k.stop, k.reset, k.quit}
+}
+
 func initModel() model {
-	return model{
+	m := model{
+		timer:   timer.New(timeout),
 		spinner: spinner.New(spinner.WithSpinner(spinner.Dot)),
-		timer:   timer.New(1 * time.Minute),
+		keymap: keymap{
+			start: key.NewBinding(key.WithKeys("s"), key.WithHelp("s", "start")),
+			stop:  key.NewBinding(key.WithKeys("s"), key.WithHelp("s", "stop")),
+			reset: key.NewBinding(key.WithKeys("r"), key.WithHelp("r", "reset")),
+			quit:  key.NewBinding(key.WithKeys("q", "esc", "ctrl+c"), key.WithHelp("q", "quit")),
+		},
+		help: help.New(),
 	}
+	m.keymap.start.SetEnabled(false)
+	return m
 }
 
 func (m model) Init() tea.Cmd {
@@ -42,18 +68,29 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "q", "esc", "ctrl+c":
-			m.quitting = true
-			return m, tea.Quit
-		default:
-			return m, nil
-		}
 	case timer.TickMsg:
 		var cmd tea.Cmd
 		m.timer, cmd = m.timer.Update(msg)
 		return m, cmd
+	case timer.StartStopMsg:
+		var cmd tea.Cmd
+		m.timer, cmd = m.timer.Update(msg)
+		m.keymap.stop.SetEnabled(m.timer.Running())
+		m.keymap.start.SetEnabled(!m.timer.Running())
+		return m, cmd
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, m.keymap.quit):
+			m.quitting = true
+			return m, tea.Quit
+		case key.Matches(msg, m.keymap.reset):
+			m.timer.Timeout = timeout
+			return m, nil
+		case key.Matches(msg, m.keymap.start, m.keymap.stop):
+			return m, m.timer.Toggle()
+		default:
+			return m, nil
+		}
 	default:
 		var cmd tea.Cmd
 		m.spinner, cmd = m.spinner.Update(msg)
@@ -62,5 +99,5 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	return fmt.Sprintf("%s%s", m.spinner.View(), m.timer.View())
+	return fmt.Sprintf("%s Existing in %s\n%s", m.spinner.View(), m.timer.View(), m.help.ShortHelpView(m.keymap.bindings()))
 }
