ddf587d2da6930c94c5eb16da7e871fdc48f41f4
sh0e1 <sh0e1@users.noreply.github.com>
2025-02-24 00:15:50 +0900

    add timer options

--- internal/cmd/timer/timer.go
+++ internal/cmd/timer/timer.go
@@ -9,17 +9,25 @@ import (
 )
 
 func NewCommand() *cobra.Command {
-	var workInterval time.Duration
+	var (
+		work       time.Duration
+		shortBreak time.Duration
+		longBreak  time.Duration
+		rounds     int
+	)
 
 	cmd := &cobra.Command{
 		Use:   "timer",
-		Short: "start timer",
+		Short: "start pomodoro timer 🍅",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return timer.Run(workInterval)
+			return timer.Run(work)
 		},
 	}
 
-	cmd.Flags().DurationVarP(&workInterval, "work-interval", "w", 25*time.Minute, "work time interval")
+	cmd.Flags().DurationVarP(&work, "work", "w", 25*time.Minute, "")
+	cmd.Flags().DurationVarP(&shortBreak, "short-break", "s", 5*time.Minute, "")
+	cmd.Flags().DurationVarP(&longBreak, "long-break", "l", 15*time.Minute, "")
+	cmd.Flags().IntVarP(&rounds, "rounds", "r", 4, "")
 
 	return cmd
 }
